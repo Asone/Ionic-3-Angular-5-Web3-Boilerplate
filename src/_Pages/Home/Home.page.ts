b04d1462befc53f1e18bcfbd71ac5b5bbd5f1878
Asone <Asone@akbarworld.info>
2018-04-19 00:25:41 +0200

    remove useless lodash library import

--- src/_Pages/Home/Home.page.ts
+++ src/_Pages/Home/Home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import filter from 'lodash-es/filter';
 import { IonicPage } from 'ionic-angular';
 import { UserModel } from '../../_Models/user.model';
 
